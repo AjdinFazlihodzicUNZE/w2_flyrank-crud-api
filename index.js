12f26f7b3f7105441aeb6534d57c501df9c6c0af
AjdinFazlihodzicUNZE <ajdin.fazlihodzic.24@size.ba>
2026-07-18 13:31:17 +0200

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 3000;
 
 
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+    res.json({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
+})
+app.get('/health',(req,res) => {
+    res.json({"status" : "ok"})
 })
 app.listen(port, () => console.log(`its alive on http://localhost:${port}`));
+
