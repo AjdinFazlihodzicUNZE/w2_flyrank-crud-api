48332f664c0f527b474d98f45588a978f29e9308
AjdinFazlihodzicUNZE <ajdin.fazlihodzic.24@size.ba>
2026-07-18 13:50:50 +0200

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const app = require('express')();
 const port = 3000;
 
+tasks = [
+    {"id" : 1,"title":"Task1","done":true},
+    {"id" : 2,"title":"Task2","done":false},
+    {"id" : 3,"title":"Task3","done":false},
+]
 
 app.get('/',(req,res)=>{
     res.json({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
 })
 app.get('/health',(req,res) => {
     res.json({"status" : "ok"})
+})
+app.get('/tasks',(req,res) => {
+    res.json(tasks);
+})
+app.get('/tasks/:id',(req,res) => {
+    const task = tasks.find(i => i.id == req.params.id)
+    if(!task){
+        return res.status(404).json({"error" : `Task ${req.params.id} not found`});
+    }
+    res.json(task);
 })
 app.listen(port, () => console.log(`its alive on http://localhost:${port}`));
 
